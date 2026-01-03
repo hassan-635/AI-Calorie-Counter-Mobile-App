2f6459018b5c74ee99845316be6f1959d41e8df6
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-01-04 00:17:12 +0500

    models and food routes added

---
+++ backend/Models/FoodLog.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const FoodLogSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  foodName: {
+    type: String,
+    required: true,
+  },
+  calories: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    default: "1 serving",
+  },
+  nutrients: {
+    protien: { type: String, default: "0g" },
+    carbs: { type: String, default: "0g" },
+    fats: { type: String, default: "0g" },
+  },
+  image: {
+    type: String, //imagge path or base64 string after gemini recognition
+  },
+  mealType: {
+    type: String,
+    enum: ["breakfast", "lunch", "dinner", "snack"],
+    default: "snack",
+  },
+  consumedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("FoodLog", FoodLogSchema);
---
+++ backend/Routes/foodRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/search-text", (req, res) => {});
+
+router.post("/analyze-text", (req, res) => {});
+
+router.get("/barcode/:barcode", (req, res) => {});
+
+module.exports = router;
