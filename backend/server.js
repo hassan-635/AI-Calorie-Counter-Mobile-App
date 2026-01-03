e4a10bf13464bb22d6d623a64e29ba72ae5a3645
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-01-03 23:16:10 +0500

    server setup error solved

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: "10mb" })); //for limit of image handling a little bt more than usual
+app.use(express.urlencoded({ extended: true }));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ DB Connection Error:", err));
+
+// // Routes Placeholders (Inhe baad mein fill karenge)
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/food", require("./routes/foodRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
